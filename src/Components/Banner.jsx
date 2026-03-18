307617be844be085dca7836596cde90d99e21421
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-19 02:42:38 +0600

    added banner heading

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-center">
+        <h1 className="font-bold text-5xl my-10">
+          We Build <br />
+          <span className="font-extrabold text-[#9F62F2]">Productive</span> Apps
+        </h1>
+        <p className="text-gray-500 my-5">
+          At HERO.IO , we craft innovative apps designed to make everyday life
+          simpler, smarter, and more exciting. Our goal is to turn your ideas
+          into digital experiences that truly make an impact.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
