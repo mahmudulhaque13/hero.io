2c7a432fd09fa138bd9001980c19c5825a73eaa9
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-18 23:21:30 +0600

    added main layout

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
+
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <Navbar></Navbar>
+      <div className="max-w-screen-2xl mx-auto w-full px-4 md:px-8 lg:px-12 flex-1">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
