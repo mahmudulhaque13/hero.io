924b141060dc498806b443635be4d8b82e894495
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-18 23:31:19 +0600

    added routes

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home";
+import Apps from "../Pages/Apps";
+import Installation from "../Pages/Installation";
+import ErrorPage from "../Pages/ErrorPage";
+import AppsDetails from "../Pages/AppsDetails";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    hydrateFallbackElement: <p>Loading...</p>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: "/apps",
+        element: <Apps></Apps>,
+      },
+      {
+        path: "/installation",
+        element: <Installation></Installation>,
+      },
+      {
+        path: "/app/:id",
+        element: <AppsDetails></AppsDetails>,
+      },
+    ],
+  },
+]);
+
+export default router;
