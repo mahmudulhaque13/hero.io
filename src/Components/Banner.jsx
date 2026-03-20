39226a239082b688e97d727bce0dfcaf5ad7e4af
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-20 17:48:14 +0600

    added play store and app store button

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import bannerImage from "../assets/hero.png";
 
 const Banner = () => {
   return (
@@ -13,6 +14,32 @@ const Banner = () => {
           simpler, smarter, and more exciting. Our goal is to turn your ideas
           into digital experiences that truly make an impact.
         </p>
+        <div className="space-x-5 mx-auto flex justify-center my-8">
+          <button className=" rounded-lg px-3 border border-gray-200">
+            <a
+              className="flex justify-center items-center font-bold"
+              href="https://play.google.com/store/games?hl=en&pli=1"
+            >
+              <img
+                src="https://img.icons8.com/fluency/48/google-play.png"
+                alt=""
+              />
+              Google Play
+            </a>
+          </button>
+          <button className=" rounded-lg px-3 border border-gray-200">
+            <a
+              className="flex justify-center items-center font-bold"
+              href="https://www.apple.com/store"
+            >
+              <img
+                src="https://img.icons8.com/fluency/48/apple-app-store.png"
+                alt=""
+              />
+              App Store
+            </a>
+          </button>
+        </div>
       </div>
     </div>
   );
