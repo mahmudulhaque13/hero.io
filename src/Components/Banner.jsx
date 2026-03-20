a4877ef7604d6327ebede03d81b42a0a2aac6e50
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-20 17:56:27 +0600

    added banner cards

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -43,6 +43,28 @@ const Banner = () => {
         <div>
           <img className="mx-auto" src={bannerImage} alt="" />
         </div>
+        <div className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white ">
+          <h1 className="font-bold  text-3xl py-5">
+            Trusted by Millions, Built for You
+          </h1>
+          <div className="flex flex-col justify-around md:flex-row">
+            <div className="text-center my-10">
+              <p>Total Downloads</p>
+              <h1 className="font-bold text-4xl">29.6M</h1>
+              <p>21% more than last month</p>
+            </div>
+            <div className="text-center my-10">
+              <p>Total Reviews</p>
+              <h1 className="font-bold text-4xl">906K</h1>
+              <p>46% more than last month</p>
+            </div>
+            <div className="text-center my-10">
+              <p>Active Apps</p>
+              <h1 className="font-bold text-4xl">132+</h1>
+              <p>31 more will Launch</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
