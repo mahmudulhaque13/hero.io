7c74eca7c33487a741d69e52e3b89ab3627937d4
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-20 18:02:45 +0600

    added trending apps section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,14 @@ const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <div className="my-10">
+        <div className="my-10">
+          <h1 className="text-center font-bold text-3xl">Trending Apps</h1>
+          <p className="text-center text-gray-500">
+            Explore All Trending Apps on the Market developed by us
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
