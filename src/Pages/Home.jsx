d52cfbd3deb8d8e0bbc6d8a1a8ff25f83244f388
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-21 23:13:01 +0600

    added show all button in home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Banner from "../Components/Banner";
+import { Link, NavLink } from "react-router";
 import AppCard from "../Components/AppCard";
+import useApps from "../Hooks/useApps";
 
 const Home = () => {
+  // const AllApps = useLoaderData();
+  const [apps, loading, error] = useApps();
+
+  const TrendingApp = apps.slice(0, 8);
   return (
     <div>
       <Banner></Banner>
@@ -14,9 +20,14 @@ const Home = () => {
           </p>
         </div>
         <div className="gap-5  grid place-items-center grid-cols-1 mx-auto md:grid-cols-2 lg:grid-cols-4">
-          {(<TrendingApp></TrendingApp>).map((app) => (
+          {TrendingApp.map((app) => (
             <AppCard key={app.id} app={app}></AppCard>
           ))}
+        </div>
+        <div className="flex justify-center items-center my-5">
+          <NavLink className="btn btn-outline bg-violet-400" to="/apps">
+            Show All
+          </NavLink>
         </div>
       </div>
     </div>
